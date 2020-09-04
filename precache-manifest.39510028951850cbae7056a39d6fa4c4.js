3b002b7457475f1b668e8eb5b127d0cbd9231389
Mieczys <ibrian_84@hotmail.com>
2020-09-04 17:18:04 -0300

    Updates

---
+++ precache-manifest.39510028951850cbae7056a39d6fa4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c506b8daceda38ab06d3f05800c8567d",
+    "url": "/Reactjs-Naranja/index.html"
+  },
+  {
+    "revision": "6c051be2a211873a0b7d",
+    "url": "/Reactjs-Naranja/static/css/main.57056714.chunk.css"
+  },
+  {
+    "revision": "650dc74009225ad07024",
+    "url": "/Reactjs-Naranja/static/js/2.a0c9310f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reactjs-Naranja/static/js/2.a0c9310f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c051be2a211873a0b7d",
+    "url": "/Reactjs-Naranja/static/js/main.80b0b346.chunk.js"
+  },
+  {
+    "revision": "ae5e1a2c10b209631ba8",
+    "url": "/Reactjs-Naranja/static/js/runtime-main.d85dae21.js"
+  }
+]);
